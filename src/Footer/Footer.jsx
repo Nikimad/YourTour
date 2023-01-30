a29d4fed19ca34f18756a8d7c61e3542ed0d2014
Nikimad <PybePoiD@gmail.com>
2023-01-30 14:17:52 +0300

    Add Footer component

---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,34 @@
+import "./Footer.css";
+import { ReactComponent as IGlogo } from "../assets/svg/instagram.svg";
+import { ReactComponent as FBlogo } from "../assets/svg/facebook.svg";
+import { ReactComponent as VKlogo } from "../assets/svg/vk.svg";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <p className="footer__text color_gray_300">Наши социальные сети</p>
+      <ul className="footer__socials">
+        <li className="footer__socials__item">
+          <IGlogo />
+          <a className="link" href="https://www.instagram.com/snp.agency/">
+            instagram
+          </a>
+        </li>
+        <li className="footer__socials__item">
+          <FBlogo />
+          <a className="link" href="http://snp.agency">
+            facebook
+          </a>
+        </li>
+        <li className="footer__socials__item">
+          <VKlogo />
+          <a className="link" href="https://vk.com/snp.agency">
+            vkontakte
+          </a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
