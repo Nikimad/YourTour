bb34a41dc44fb2a3926024cc9bedff46c972e7e6
Nikimad <PybePoiD@gmail.com>
2023-01-31 00:06:22 +0300

    Add CreateTour component

---
+++ src/CreateTour/CreateTour.jsx
@@ -0,0 +1,12 @@
+import Section from "../Section/Section";
+import FormContainer from '../Form/FormContainer';
+
+const CreateTour = () => {
+    return (
+        <Section id="create-tour">
+            <FormContainer/>
+        </Section>
+    );
+};
+
+export default CreateTour;
