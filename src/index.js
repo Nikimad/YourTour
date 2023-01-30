8a33b19575c6753abf7dfdd7a06bd247d68f3264
Nikimad <PybePoiD@gmail.com>
2023-01-30 14:18:43 +0300

    Update index.js: Add Header and Footer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './styles/common.css';
+import Header from './Header/Header';
+import Footer from './Footer/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-
+    <Header/>
+    <Footer/>
   </React.StrictMode>
 );
