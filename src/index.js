c69e30e26511cb31af9385afadb3302430faaf09
Nikimad <PybePoiD@gmail.com>
2023-01-31 00:06:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './styles/common.css';
 import Header from './Header/Header';
 import Reviews from './Reviews/Reviews';
+import CreateTour from './CreateTour/CreateTour';
 import Footer from './Footer/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Header/>
+    <CreateTour/>
     <Reviews/>
     <Footer/>
   </React.StrictMode>
