fe11c66415bfa05d3a0b1efdef722679914d72c6
Nikimad <PybePoiD@gmail.com>
2023-01-31 10:49:04 +0300

    Add opotunity to mix classnames for LinkMore

--- src/LinkMore/LinkMore.jsx
+++ src/LinkMore/LinkMore.jsx
@@ -1,9 +1,12 @@
 import "./LinkMore.css";
+import cn from "classnames";
 import { ReactComponent as ArrowIcon } from "../assets/svg/arrow.svg";
 
 const LinkMore = (props) => {
+  const {className} = props;
+  
   return (
-    <a {...props} className="link-more">
+    <a {...props} className={cn("link-more", className)}>
       <span className="fs_big">Подробнее</span>
       <ArrowIcon />
     </a>
