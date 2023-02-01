800df89625115cbc77e8c491b8913af091c9a55c
Nikimad <PybePoiD@gmail.com>
2023-02-01 12:44:05 +0300

    Update Section component, add styles and header option

--- src/Section/Section.jsx
+++ src/Section/Section.jsx
@@ -1,7 +1,20 @@
-const Section = (props) => {
-  const { children } = props;
+import './Section.css';
 
-  return <section className="section">{children}</section>;
+const Section = (props) => {
+  const { title, subtitle, id, children } = props;
+  const isHeaderCustom = typeof title === "string";
+
+  return (
+    <section className="section" id={id}>
+      {isHeaderCustom && (
+        <div className="section__header">
+          <h2 className="h2">{title}</h2>
+          <p className="color_gray_300">{subtitle}</p>
+        </div>
+      )}
+      {children}
+    </section>
+  );
 };
 
 export default Section;
