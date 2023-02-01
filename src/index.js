fb8504836a1b7271eec50b92998b62c6dca9dd2b
Nikimad <PybePoiD@gmail.com>
2023-02-01 12:48:16 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Reviews from './Reviews/Reviews';
 import CreateTour from './CreateTour/CreateTour';
 import Gallery from './Gallery/Gallery';
 import Stories from './Stories/Stories';
+import Cta from './Cta/Cta';
 import Footer from './Footer/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -19,6 +20,7 @@ root.render(
     <Reviews/>
     <Gallery/>
     <Stories/>
+    <Cta/>
     <Footer/>
   </React.StrictMode>
 );
