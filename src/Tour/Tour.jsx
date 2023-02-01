82668311cc762ac4a9600adf9df560f4f0f9be5d
Nikimad <PybePoiD@gmail.com>
2023-02-01 15:38:33 +0300

    Update Tour title width

--- src/Tour/Tour.jsx
+++ src/Tour/Tour.jsx
@@ -7,7 +7,7 @@ const Tour = (props) => {
 
   return (
     <article className="tour link-more-container cover-container br_main">
-      <h3 className="h3">{title}</h3>
+      <h3 className="h3 tour__title">{title}</h3>
       <p className="fs_small">{price}</p>
       <LinkMore href={linkHref} className="tour__link" />
       <Cover src={coverImg} />
@@ -16,7 +16,7 @@ const Tour = (props) => {
 };
 
 Tour.defaultProps = {
-  title: "Путешествие в горы",
+  title: `Путешествие в горы`,
   price: "от 80 000 руб",
 };
 
