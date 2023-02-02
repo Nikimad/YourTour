d834af79c610ac7593a0981eeff710d91973e3ea
Nikimad <PybePoiD@gmail.com>
2023-02-02 16:01:46 +0300

    Update CreateTourComponent

--- src/components/CreateTour/CreateTour.jsx
+++ src/components/CreateTour/CreateTour.jsx
@@ -1,17 +1,15 @@
 import Section from "../Section";
 import Form from "../Form";
 
-const CreateTour = () => {
-  return (
-    <Section
-      id="create-tour"
-      title="Собери свой тур"
-      subtitle="Идейные соображения высшего порядка, а также рамки и место обучения кадров"
-      className="container"
-    >
-      <Form />
-    </Section>
-  );
-};
+const CreateTour = () => (
+  <Section
+    id="create-tour"
+    title="Собери свой тур"
+    subtitle="Идейные соображения высшего порядка, а также рамки и место обучения кадров"
+    className="container"
+  >
+    <Form />
+  </Section>
+);
 
 export default CreateTour;
