58628daa237f285a7b4143035aacde6b6b2334e9
Nikimad <PybePoiD@gmail.com>
2023-02-02 15:50:06 +0300

    Update App component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,14 +2,12 @@ import Header from "../Header";
 import Main from "../Main";
 import Footer from "../Footer";
 
-const App = () => {
-  return (
-    <>
-      <Header />
-      <Main />
-      <Footer />
-    </>
-  );
-};
+const App = () => (
+  <>
+    <Header />
+    <Main />
+    <Footer />
+  </>
+);
 
 export default App;
