491add572a3e78fad3da667dae0040648f312a57
Nikimad <PybePoiD@gmail.com>
2023-02-02 13:57:08 +0300

    Update LinkMore.jsx

--- src/LinkMore/LinkMore.jsx
+++ src/LinkMore/LinkMore.jsx
@@ -3,10 +3,8 @@ import cn from "classnames";
 import { ReactComponent as ArrowIcon } from "../assets/svg/arrow.svg";
 
 const LinkMore = (props) => {
-  const { className } = props;
-
   return (
-    <a {...props} className={cn("link-more", className)}>
+    <a {...props} className={cn("link-more", props.className)}>
       <span className="fs_big">Подробнее</span>
       <ArrowIcon />
     </a>
