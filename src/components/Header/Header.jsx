12284777ecf3108708efa0c975ea993f4f6085fc
Nikimad <PybePoiD@gmail.com>
2023-02-02 16:02:49 +0300

    Update Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,20 @@
 import "./Header.css";
 import Navbar from "../Navbar";
 
-const Header = () => {
-  return (
-    <header className="header">
-      <Navbar />
-      <div className="container header__container">
-        <h1 className="h1">Идеальные путешествия существуют</h1>
-        <p>
-          Идейные соображения высшего порядка, а также рамки и место обучения
-          кадров
-        </p>
-        <a href="#gallery" className="header__link fs_small br_10">
-          Найти тур
-        </a>
-      </div>
-    </header>
-  );
-};
+const Header = () => (
+  <header className="header">
+    <Navbar />
+    <div className="container header__container">
+      <h1 className="h1">Идеальные путешествия существуют</h1>
+      <p>
+        Идейные соображения высшего порядка, а также рамки и место обучения
+        кадров
+      </p>
+      <a href="#gallery" className="header__link fs_small br_10">
+        Найти тур
+      </a>
+    </div>
+  </header>
+);
 
 export default Header;
