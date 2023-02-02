0fb5cfe3dcda5d1886df6ab74123c3012887a0e0
Nikimad <PybePoiD@gmail.com>
2023-02-02 16:03:07 +0300

    Update LinkMore component

--- src/components/LinkMore/LinkMore.jsx
+++ src/components/LinkMore/LinkMore.jsx
@@ -2,13 +2,11 @@ import "./LinkMore.css";
 import cn from "classnames";
 import { ReactComponent as ArrowIcon } from "../../assets/svg/arrow.svg";
 
-const LinkMore = (props) => {
-  return (
-    <a {...props} className={cn("link-more", props.className)}>
-      <span className="fs_big">Подробнее</span>
-      <ArrowIcon />
-    </a>
-  );
-};
+const LinkMore = (props) => (
+  <a {...props} className={cn("link-more", props.className)}>
+    <span className="fs_big">Подробнее</span>
+    <ArrowIcon />
+  </a>
+);
 
 export default LinkMore;
