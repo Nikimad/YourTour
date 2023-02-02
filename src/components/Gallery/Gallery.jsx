9246aef8cf58213e6a88419b86166b8ca25ef0ba
Nikimad <PybePoiD@gmail.com>
2023-02-02 16:02:34 +0300

    Update Gallery component

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -14,84 +14,82 @@ import travel11 from "../../assets/images/travel-11.jpg";
 import travel12 from "../../assets/images/travel-12.jpg";
 import travel13 from "../../assets/images/travel-13.jpg";
 
-const Gallery = () => {
-  return (
-    <Section
-      id="gallery"
-      title="Фотографии путешествий"
-      subtitle="Идейные соображения высшего порядка, а также рамки и место обучения кадров"
-    >
-      <div className="gallery">
-        <div className="gallery__row">
-          <img
-            className="gallery__img br_main"
-            src={travel1}
-            alt="Самолёт над заливом"
-          />
-          <img
-            className="gallery__img gallery__img_hide-tab br_main"
-            src={travel2}
-            alt="Кэмпинг в пустыне"
-          />
-          <img
-            className="gallery__img gallery__img_hide-mob  br_main"
-            src={travel3}
-            alt="Горная прогулка"
-          />
-          <img
-            className="gallery__img br_main"
-            src={travel4}
-            alt="Пустая дорога"
-          />
-        </div>
-        <div className="gallery__row">
-          <img
-            className="gallery__img br_main"
-            src={travel5}
-            alt="Скалы у моря"
-          />
-          <img
-            className="gallery__img gallery__img_hide-mob br_main"
-            src={travel6}
-            alt="Карта"
-          />
-          <img
-            className="gallery__img gallery__img_hide-tab br_main"
-            src={travel7}
-            alt="Мост в лесу"
-          />
-          <img className="gallery__img br_main" src={travel8} alt="Кэмпинг" />
-          <img
-            className="gallery__img br_main"
-            src={travel13}
-            alt="Узкие улицы"
-          />
-        </div>
-        <div className="gallery__row">
-          <img
-            className="gallery__img br_main"
-            src={travel9}
-            alt="Старый город"
-          />
-          <img
-            className="gallery__img br_main"
-            src={travel10}
-            alt="Озеро в горах"
-          />
-          <img
-            className="gallery__img gallery__img_hide-mob br_main"
-            src={travel11}
-            alt="Оживлённая улица"
-          />
-          <img
-            className="gallery__img gallery__img_hide-tab br_main"
-            src={travel12}
-            alt="Сапсёрфинг"
-          />
-        </div>
+const Gallery = () => (
+  <Section
+    id="gallery"
+    title="Фотографии путешествий"
+    subtitle="Идейные соображения высшего порядка, а также рамки и место обучения кадров"
+  >
+    <div className="gallery">
+      <div className="gallery__row">
+        <img
+          className="gallery__img br_main"
+          src={travel1}
+          alt="Самолёт над заливом"
+        />
+        <img
+          className="gallery__img gallery__img_hide-tab br_main"
+          src={travel2}
+          alt="Кэмпинг в пустыне"
+        />
+        <img
+          className="gallery__img gallery__img_hide-mob  br_main"
+          src={travel3}
+          alt="Горная прогулка"
+        />
+        <img
+          className="gallery__img br_main"
+          src={travel4}
+          alt="Пустая дорога"
+        />
       </div>
-    </Section>
-  );
-};
+      <div className="gallery__row">
+        <img
+          className="gallery__img br_main"
+          src={travel5}
+          alt="Скалы у моря"
+        />
+        <img
+          className="gallery__img gallery__img_hide-mob br_main"
+          src={travel6}
+          alt="Карта"
+        />
+        <img
+          className="gallery__img gallery__img_hide-tab br_main"
+          src={travel7}
+          alt="Мост в лесу"
+        />
+        <img className="gallery__img br_main" src={travel8} alt="Кэмпинг" />
+        <img
+          className="gallery__img br_main"
+          src={travel13}
+          alt="Узкие улицы"
+        />
+      </div>
+      <div className="gallery__row">
+        <img
+          className="gallery__img br_main"
+          src={travel9}
+          alt="Старый город"
+        />
+        <img
+          className="gallery__img br_main"
+          src={travel10}
+          alt="Озеро в горах"
+        />
+        <img
+          className="gallery__img gallery__img_hide-mob br_main"
+          src={travel11}
+          alt="Оживлённая улица"
+        />
+        <img
+          className="gallery__img gallery__img_hide-tab br_main"
+          src={travel12}
+          alt="Сапсёрфинг"
+        />
+      </div>
+    </div>
+  </Section>
+);
 
 export default Gallery;
