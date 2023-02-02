cef8dbee83325f1f4a8ab74dff63c81e25a80cb7
Nikimad <PybePoiD@gmail.com>
2023-02-02 16:04:47 +0300

    Update Tour component

--- src/components/Tour/Tour.jsx
+++ src/components/Tour/Tour.jsx
@@ -2,16 +2,14 @@ import "./Tour.css";
 import Cover from "../Cover";
 import LinkMore from "../LinkMore";
 
-const Tour = ({ title, price, linkHref, coverImg }) => {
-  return (
-    <article className="tour link-more-container cover-container br_main">
-      <h3 className="h3 tour__title">{title}</h3>
-      <p className="fs_small">{price}</p>
-      <LinkMore href={linkHref} className="tour__link" />
-      <Cover src={coverImg} />
-    </article>
-  );
-};
+const Tour = ({ title, price, linkHref, coverImg }) => (
+  <article className="tour link-more-container cover-container br_main">
+    <h3 className="h3 tour__title">{title}</h3>
+    <p className="fs_small">{price}</p>
+    <LinkMore href={linkHref} className="tour__link" />
+    <Cover src={coverImg} />
+  </article>
+);
 
 Tour.defaultProps = {
   title: `Путешествие в горы`,
