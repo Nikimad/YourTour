06f456637028fcb095bd8dde8975a4abdb01916b
Nikimad <PybePoiD@gmail.com>
2023-02-02 16:03:41 +0300

    Update Review component

--- src/components/Review/Review.jsx
+++ src/components/Review/Review.jsx
@@ -1,27 +1,25 @@
 import "./Review.css";
 import { uniqueId } from "lodash";
 
-const Review = ({ authorName, texts, tour, authorAvatar }) => {
-  return (
-    <article className="review bg_beige_200 br_main">
-      <div className="review__body">
-        {texts.map((text) => (
-          <p key={uniqueId()} className="fs_big">
-            {text}
-          </p>
-        ))}
-      </div>
-      <div className="review__footer">
-        <h3 className="h3">{authorName}</h3>
-        <p className="fs_small color_gray_300">{`Тур: ${tour}`}</p>
-        <img
-          className="review__avatar"
-          src={authorAvatar}
-          alt={`${authorName} аватар`}
-        />
-      </div>
-    </article>
-  );
-};
+const Review = ({ authorName, texts, tour, authorAvatar }) => (
+  <article className="review bg_beige_200 br_main">
+    <div className="review__body">
+      {texts.map((text) => (
+        <p key={uniqueId()} className="fs_big">
+          {text}
+        </p>
+      ))}
+    </div>
+    <div className="review__footer">
+      <h3 className="h3">{authorName}</h3>
+      <p className="fs_small color_gray_300">{`Тур: ${tour}`}</p>
+      <img
+        className="review__avatar"
+        src={authorAvatar}
+        alt={`${authorName} аватар`}
+      />
+    </div>
+  </article>
+);
 
 export default Review;
