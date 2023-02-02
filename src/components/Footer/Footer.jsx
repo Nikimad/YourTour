700f8bb6dd309b6c65b109e1d29954e2fad7d83b
Nikimad <PybePoiD@gmail.com>
2023-02-02 16:02:16 +0300

    Update Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,32 +3,30 @@ import { ReactComponent as IGlogo } from "../../assets/svg/instagram.svg";
 import { ReactComponent as FBlogo } from "../../assets/svg/facebook.svg";
 import { ReactComponent as VKlogo } from "../../assets/svg/vk.svg";
 
-const Footer = () => {
-  return (
-    <footer className="footer container">
-      <p className="footer__text color_gray_300">Наши социальные сети</p>
-      <ul className="footer__socials">
-        <li className="footer__socials__item">
-          <IGlogo />
-          <a className="link" href="https://www.instagram.com/snp.agency/">
-            instagram
-          </a>
-        </li>
-        <li className="footer__socials__item">
-          <FBlogo />
-          <a className="link" href="http://snp.agency">
-            facebook
-          </a>
-        </li>
-        <li className="footer__socials__item">
-          <VKlogo />
-          <a className="link" href="https://vk.com/snp.agency">
-            vkontakte
-          </a>
-        </li>
-      </ul>
-    </footer>
-  );
-};
+const Footer = () => (
+  <footer className="footer container">
+    <p className="footer__text color_gray_300">Наши социальные сети</p>
+    <ul className="footer__socials">
+      <li className="footer__socials__item">
+        <IGlogo />
+        <a className="link" href="https://www.instagram.com/snp.agency/">
+          instagram
+        </a>
+      </li>
+      <li className="footer__socials__item">
+        <FBlogo />
+        <a className="link" href="http://snp.agency">
+          facebook
+        </a>
+      </li>
+      <li className="footer__socials__item">
+        <VKlogo />
+        <a className="link" href="https://vk.com/snp.agency">
+          vkontakte
+        </a>
+      </li>
+    </ul>
+  </footer>
+);
 
 export default Footer;
