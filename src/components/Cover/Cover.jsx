b5c530c3c62933e58d6202259513e5bba3f5c2a5
Nikimad <PybePoiD@gmail.com>
2023-09-15 19:29:18 +0300

    Change /src/components/Cover

--- src/components/Cover/Cover.jsx
+++ src/components/Cover/Cover.jsx
@@ -1,8 +1,9 @@
+import Image from "next/image";
 import "./Cover.css";
 
 const Cover = ({ src }) => (
   <div className="cover" aria-hidden="true">
-    <img src={src} className="cover__img" alt="" />
+    <Image src={src} fill sizes="100%" className="cover__img" alt="" />
   </div>
 );
 
